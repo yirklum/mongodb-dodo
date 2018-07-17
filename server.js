a52fe4b12d743c55a6150d218f3a0040b0dd60ab
Yirklum <yirklum@aol.com>
2018-07-16 23:27:05 -0400

    deploy

--- server.js
+++ server.js
@@ -11,15 +11,20 @@ var axios = require("axios");
 // Require models
 var db = require("./models");
 
-// Connect to the Mongo DB
-mongoose.Promise = Promise;
-mongoose.connect("mongodb://heroku_ztzgq858:Ihatebutterflies1@ds139921.mlab.com:39921/heroku_ztzgq858");
+// Port configuration for local/Heroku
+var PORT = process.env.PORT || process.argv[2] || 8080;
 
+// Connect to the Mongo DB
 var MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+
 
 
 // Choose port
-var PORT = 3000;
+// var PORT = 3000;
 
 // Initialize Express
 var app = express();
